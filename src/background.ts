69e5ca9a2822322a8170048e2ae407dbdd96873e
amy <ctrlalternativedelete@gmail.com>
2022-02-09 11:40:04 +1100

    added lowdb stuff

--- src/background.ts
+++ src/background.ts
@@ -132,3 +132,22 @@ ipcMain.on('dialog-open', (event) => {
     event.returnValue = null
   } 
 })
+
+// import low from 'lowdb';
+// import FileSync from 'lowdb/adapters/FileSync';
+
+// const adapter = new FileSync('db.json')
+// const db = low(adapter)
+ 
+// // Set some defaults
+// db.defaults({ posts: [], user: {} })
+//   .write()
+ 
+// // Add a post
+// db.get('posts')
+//   .push({ id: 1, title: 'lowdb is awesome'})
+//   .write()
+ 
+// // Set a user using Lodash shorthand syntax
+// db.set('user.name', 'typicode')
+//   .write()
