bbcfa0124ad35325e62c6717e8456be0256b4d99
amy <ctrlalternativedelete@gmail.com>
2022-02-18 09:25:50 +1100

    added title fix

--- src/components/Reminders/RemindersContainer.vue
+++ src/components/Reminders/RemindersContainer.vue
@@ -5,7 +5,7 @@
             <ReminderEditor v-model="sortedReminders[i]" v-slot="slotProp" :key="i">
                 <v-list-item v-if="!slotProp.opened">
                     <v-list-item-content>
-                        <v-list-item-title>{{reminder.title}}</v-list-item-title>
+                        <v-list-item-title>{{reminder.title ? reminder.title : "No Title"}}</v-list-item-title>
                         <v-list-item-subtitle>{{new Date(reminder.date).toLocaleString()}}</v-list-item-subtitle>
                         <v-list-item-subtitle v-if="reminder.details.length !== 0" v-html="md.render(reminder.details)"/>
 
