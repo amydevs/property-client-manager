cf01edc648f66249bd8bd198e19fb9712f8493e7
amy <ctrlalternativedelete@gmail.com>
2022-02-21 10:52:33 +1100

    added docs to main

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import { ClientsDB } from './modules/ClientsDB'
 
-
+// Expose backend interfacing functions in frontend.
 Vue.prototype.$electron = window.electron
 
+// Expose database handler in frontend.
 import alt from './store/alt'
 Vue.prototype.$altStore = alt
 declare module 'vue/types/vue' {
@@ -25,6 +26,7 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
+// Declare types for interface to interact with backend.
 declare global {
   interface Window {
     electron: {
