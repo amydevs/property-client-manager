2b34d71d2060655f20c1a8713e2d9e4517e3c59f
Amy Y <ctrlalternativedelete@gmail.com>
2022-05-19 20:15:05 +1000

    added comments into store

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,10 +4,12 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+// Defines the type of the database storage accessible in the frontend.
 export interface State {
   clientsdb: ClientsDB | null
 }
 
+// Initialize the database storage accessible in the frontend.
 const store = new Vuex.Store<State>({
   state: {
     clientsdb: window.electron.clients.get()
@@ -20,6 +22,7 @@ const store = new Vuex.Store<State>({
   }
 })
 
+// Exports static helper functions to access the database in the frontend.
 export class StoreUtils {
   static findClientById(id:string) {
     return store.state.clientsdb?.clients.find(client => client.id === id);
@@ -29,48 +32,22 @@ export class StoreUtils {
   }
 }
 
+// initializes a variable with null value, that is to be used to store the id's of timers
 let timer = null as number | null;
-
 store.watch( (state, getters) => JSON.stringify(state.clientsdb), (newValue, oldValue) => {
+  // if a timer's id is stored in cache, cancel the execution of the function that is to be run when the timer reaches zero.
   if (timer) {
     clearTimeout(timer);
   }
+
+  // create a new timer that tells the backend to write changes the database's changes to disk when it reaches zero.
   timer = window.setTimeout(() => {
-    console.log("help")
     window.electron.clients.write(store.state.clientsdb as ClientsDB);
   }, 50) 
 });
 
 window.electron.ipc.receive("clients-changed", (event:any) => {
-  // console.log("clientsdb received")
   store.state.clientsdb = event;
 });
-
-// export const StoreManager = new Vue({
-//   // reactive getter that reads the clientsdb global variable as a string
-//   computed: {
-//     storestate(): ClientsDB {
-//       return store.state.clientsdb as ClientsDB;
-//     },
-//   },
-//   // watch the string value of clientsdb for changes, if changes are detected, tell the backend to write to the clients database file.
-//   watch: {
-//     storestate: {
-//       deep: true,
-//       handler(val: ClientsDB) {
-//         if (val != oldval) {
-//           console.log("store changed")
-//           window.electron.clients.write(store.state.clientsdb as ClientsDB)
-//         }
-//       }
-//     }
-//   },
-//   // when a change is detected in the clients database file, the clientsdb global variable is refreshed with new information.
-//   created() {
-//     window.electron.ipc.receive("clients-changed", (event:any) => {
-//       store.state.clientsdb = event;
-//     });
-//   }
-// });
 
 export default store;
